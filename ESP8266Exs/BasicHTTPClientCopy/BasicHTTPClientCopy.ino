8ef81ba75da7c314d9d3988a72ff168e848e7f72
Hend Adel <hend.adel.m@gmail.com>
2019-05-08 11:01:03 +0200

    call google.com website commit, first commit after first stash

---
+++ ESP8266Exs/BasicHTTPClientCopy/BasicHTTPClientCopy.ino
@@ -0,0 +1,87 @@
+/**
+   BasicHTTPClient.ino
+
+    Created on: 24.05.2015
+
+*/
+
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <ESP8266HTTPClient.h>
+
+#include <WiFiClient.h>
+
+ESP8266WiFiMulti WiFiMulti;
+
+//const char* ssid = "Focus";
+//const char* password = "Focus@Pro";
+
+const char* ssid = "Mi Phone";
+const char* password = "0100@Baba579";
+
+void setup() {
+
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    //The flush() method gives a chance to wait until all Serial.print()ing is done.
+    Serial.flush();
+    delay(1000);
+  }
+
+  // WiFi mode to chose if the bord will be station -device- with WIFI_STA option or access point with WIFI_AP option.
+  WiFi.mode(WIFI_STA);
+  // Add the access point setting
+  WiFiMulti.addAP(ssid, password);
+
+  // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+
+    WiFiClient client;
+
+    HTTPClient http;
+
+    Serial.print("[HTTP] begin...\n");
+    if (http.begin(client, "http://www.google.com")) {  // HTTP
+
+
+      Serial.print("[HTTP] GET...\n");
+      // start connection and send HTTP header
+      int httpCode = http.GET();
+
+      // httpCode will be negative on error
+      if (httpCode > 0) {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+        // file found at server
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          String payload = http.getString();
+          Serial.println(payload);
+        }
+      } else {
+        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      }
+
+      http.end();
+    } else {
+      Serial.printf("[HTTP} Unable to connect\n");
+    }
+  }
+
+  delay(10000);
+  
+}
+
+void loop() {
+  
+}
