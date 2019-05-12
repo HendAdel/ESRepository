e76760f4a44ce22f9226f3aa932e06913105b706
Hend Adel <hend.adel.m@gmail.com>
2019-05-12 16:44:40 +0200

    Authorization example commit

--- ESP8266Exs/AuthorizationCopy/AuthorizationCopy.ino
+++ ESP8266Exs/AuthorizationCopy/AuthorizationCopy.ino
@@ -13,20 +13,20 @@
 #include <ESP8266HTTPClient.h>
 
 #include <WiFiClient.h>
-#include <WiFiClientSecureBearSSL.h>
+//#include <WiFiClientSecureBearSSL.h>
 
 // Fingerprint for demo URL, expires on June 2, 2019, needs to be updated well before this date    
-const uint8_t fingerprint[20] = {0x14, 0xEC, 0x6C, 0x23, 0x68, 0x9B, 0x7D, 0xAE, 0x55, 0x70, 0xCB, 0x13, 0x8E, 0x01, 0xD4, 0xEF, 0xB2, 0x12, 0x41, 0x2D};
+//const uint8_t fingerprint[20] = {0x14, 0xEC, 0x6C, 0x23, 0x68, 0x9B, 0x7D, 0xAE, 0x55, 0x70, 0xCB, 0x13, 0x8E, 0x01, 0xD4, 0xEF, 0xB2, 0x12, 0x41, 0x2D};
 
 
 ESP8266WiFiMulti WiFiMulti;
 
 
-const char* ssid = "Focus";
-const char* password = "Focus@Pro";
+//const char* ssid = "Focus";
+//const char* password = "Focus@Pro";
 
-//const char* ssid = "Mi Phone";
-//const char* password = "0100@Baba579";
+const char* ssid = "Mi Phone";
+const char* password = "0100@Baba579";
 
 void setup() {
 
@@ -52,40 +52,49 @@ void loop() {
   // wait for WiFi connection
   if ((WiFiMulti.run() == WL_CONNECTED)) {
 
-    //WiFiClient client;
+    WiFiClient client;
 
-   std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+  // std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
 
-    client->setFingerprint(fingerprint);
+    //client->setFingerprint(fingerprint);
 
-    HTTPClient https;
+   // HTTPClient https;
 
-   // HTTPClient http;
+    Serial.println("");
+    Serial.print("Connected! IP address: ");
+    Serial.println(WiFi.localIP());
+    
+    HTTPClient http;
     Serial.print("[HTTP] begin...\n");
     // configure traged server and url
-
-
+    // http.begin(client, "10.2.101.103");
+    http.begin(client, "http://192.168.43.58/iisstart.htm");
+    http.setAuthorization("guest", "guest");
+    
     //http.begin(client, "http://guest:guest@jigsaw.w3.org/HTTP/Basic/");
 
     
       // or
       //https.begin(*client, "https://jigsaw.w3.org/HTTP/connection.html");
-      https.begin(*client, "https://accounts.google.com");
-      https.setAuthorization("hend.adel", "D15He4I20Y19nd");
-
-  /*    http.begin(client, "http://jigsaw.w3.org/HTTP/Basic/");
-      http.setAuthorization("guest", "guest");
       
- 
+      //or
+      //https.begin(*client, "https://accounts.google.com");
+     // https.setAuthorization("hend.adel", "D15He4I20Y19nd");
+ /*
+      http.begin(client, "http://jigsaw.w3.org/HTTP/Basic/");
+      http.setAuthorization("guest", "guest");
+   
+
       // or
       http.begin(client, "http://jigsaw.w3.org/HTTP/Basic/");
       http.setAuthorization("Z3Vlc3Q6Z3Vlc3Q=");
-   */
+      http.setAuthentication   
+    */  
 
 
     Serial.print("[HTTP] GET...\n");
     // start connection and send HTTP header
-    int httpCode = https.GET();
+    int httpCode = http.GET();
 
     // httpCode will be negative on error
     if (httpCode > 0) {
@@ -94,14 +103,14 @@ void loop() {
 
       // file found at server
       if (httpCode == HTTP_CODE_OK) {
-        String payload = https.getString();
+        String payload = http.getString();
         Serial.println(payload);
       }
     } else {
-      Serial.printf("[HTTP] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
     }
 
-    https.end();
+    http.end();
   }
 
   delay(10000);
